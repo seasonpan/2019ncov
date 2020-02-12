5a3cc40371976610c5e0f506afe435e715f7a76e
shichenpan <sc.pan@qq.com>
2020-02-12 15:35:54 +0800

    deploy

---
+++ precache-manifest.bb0662c2296e64163a641b364993706b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b60845c2c0d5f29a8fe380d8c2bccf30",
+    "url": "/2019ncov/index.html"
+  },
+  {
+    "revision": "f37e433def461d113c8c",
+    "url": "/2019ncov/static/css/2.9ddb28ea.chunk.css"
+  },
+  {
+    "revision": "f37e433def461d113c8c",
+    "url": "/2019ncov/static/js/2.f7063a3a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/2019ncov/static/js/2.f7063a3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "105b53b58c21c76b59bf",
+    "url": "/2019ncov/static/js/main.5af9b9f1.chunk.js"
+  },
+  {
+    "revision": "96134a8e51e6f2c0d702",
+    "url": "/2019ncov/static/js/runtime-main.9283c2dc.js"
+  },
+  {
+    "revision": "1dc15fddda316a32aac82bdfaa6b6256",
+    "url": "/2019ncov/static/media/bg.1dc15fdd.jpg"
+  }
+]);
